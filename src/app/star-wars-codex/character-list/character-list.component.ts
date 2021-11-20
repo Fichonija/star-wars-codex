ee555864d9ca3390e50be0f5e1344bd23e1434b9
Filip Bolčić <fbolcic@span.eu>
2021-11-20 15:42:12 +0100

    Render TableComponent in CharacterListComponent

--- src/app/star-wars-codex/character-list/character-list.component.ts
+++ src/app/star-wars-codex/character-list/character-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ChangeDetectorRef,
+  OnDestroy,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TableData } from 'src/app/table/table-data.model';
 import { CharactersResponse } from '../models/character.model';
 import { StarWarsService } from '../star-wars.service';
 
@@ -8,10 +16,41 @@ import { StarWarsService } from '../star-wars.service';
   styleUrls: ['./character-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CharacterListComponent implements OnInit {
-  constructor(public starWarsService: StarWarsService) {}
+export class CharacterListComponent implements OnInit, OnDestroy {
+  private characterResponse$: Subscription | undefined;
+
+  public charactersResponse: CharactersResponse | undefined;
+  public charactersTableData: TableData | undefined;
+
+  constructor(
+    private starWarsService: StarWarsService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.starWarsService.fetchPeople();
+    this.characterResponse$ = this.starWarsService
+      .fetchPeople()
+      .subscribe((charactersResponse) => {
+        this.charactersResponse = charactersResponse;
+        this.charactersTableData =
+          this.mapCharactersTableData(charactersResponse);
+        this.changeDetectorRef.markForCheck();
+      });
+  }
+
+  private mapCharactersTableData(
+    charactersResponse: CharactersResponse
+  ): TableData {
+    let charactersTableData: TableData = {
+      columns: [{ accessor: 'name', label: 'Name' }],
+      rows: charactersResponse.characters.map((character) => {
+        return { name: character.name };
+      }),
+    };
+    return charactersTableData;
+  }
+
+  ngOnDestroy() {
+    this.characterResponse$?.unsubscribe();
   }
 }
